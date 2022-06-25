38369adaed90473eda55461100d8cfb548b703d8
Caio de Sousa (Sony) <sonyhellish@gmail.com>
2022-06-25 05:49:06 -0300

    [feat] Adds the ExploreDrinksByIngredients page
    Same as the previous commit. Just copying the ExploreFoodsByIngredients page and pasting, changing API function, img src and component function name.

--- src/pages/Explore/ExploreDrinksBy/ExploreDrinksByIngredients.jsx
+++ src/pages/Explore/ExploreDrinksBy/ExploreDrinksByIngredients.jsx
@@ -1,15 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../../../components/Header/Header';
+import IngredientCard from '../../../components/IngredientCard/IngredientCard';
 import MenuInferior from '../../../components/menu inferior/MenuInferior';
+import { fetchingExploreDrinksIngredients } from '../../../services/ExploreAPI';
 import './ExploreDrinksBy.css';
 
-function ExploreDrinksByIngredients() {
+const ExploreDrinksByIngredients = () => {
+  const [ingredients, setIngredients] = useState([]);
+
+  useEffect(() => {
+    const fetchIngredients = async () => {
+      const ingredientsNames = await fetchingExploreDrinksIngredients();
+      setIngredients(ingredientsNames);
+      console.log(ingredientsNames);
+    };
+    fetchIngredients();
+  }, []);
+
   return (
     <div>
       <Header />
+      <section className="ExploreIngredientSection">
+        { ingredients.map((e, index) => (
+          <button
+            type="button"
+            key={ e }
+            className="ExploreIngredientButton"
+          >
+            <IngredientCard
+              name={ e }
+              img={ `https://www.thecocktaildb.com/images/ingredients/${e}-Small.png` }
+              index={ index }
+            />
+          </button>
+        ))}
+      </section>
       <MenuInferior />
     </div>
   );
-}
+};
 
 export default ExploreDrinksByIngredients;
