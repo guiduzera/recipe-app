53c1681f7857a04f5acb850ef3152a289d2968ff
Caio de Sousa (Sony) <sonyhellish@gmail.com>
2022-06-25 05:44:00 -0300

    [feat] Adds the ExploreFoodsByIngredients page
    Adding the content on ExploreFoodsByIngredients. Consuming the API on ExploreAPI, saving the ingredients on a state, rendering multiple cards for each ingredient.

--- src/pages/Explore/ExploreFoodsBy/ExploreFoodsByIngredients.jsx
+++ src/pages/Explore/ExploreFoodsBy/ExploreFoodsByIngredients.jsx
@@ -1,15 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../../../components/Header/Header';
+import IngredientCard from '../../../components/IngredientCard/IngredientCard';
 import MenuInferior from '../../../components/menu inferior/MenuInferior';
+import { fetchingExploreFoodsIngredients } from '../../../services/ExploreAPI';
 import './ExploreFoodsBy.css';
 
-function ExploreFoodsByIngredients() {
+const ExploreFoodsByIngredients = () => {
+  const [ingredients, setIngredients] = useState([]);
+
+  useEffect(() => {
+    const fetchIngredients = async () => {
+      const ingredientsNames = await fetchingExploreFoodsIngredients();
+      setIngredients(ingredientsNames);
+      console.log(ingredientsNames);
+    };
+    fetchIngredients();
+  }, []);
+
   return (
     <div>
       <Header />
+      <section className="ExploreIngredientSection">
+        { ingredients.map((e, index) => (
+          <button
+            type="button"
+            key={ e }
+            className="ExploreIngredientButton"
+          >
+            <IngredientCard
+              name={ e }
+              img={ `https://www.themealdb.com/images/ingredients/${e}-Small.png` }
+              index={ index }
+            />
+          </button>
+        ))}
+      </section>
       <MenuInferior />
     </div>
   );
-}
+};
 
 export default ExploreFoodsByIngredients;
